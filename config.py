a0fb43b219c61e5f987a91e60ec76386e0ba9be0
pigpig1524 <vantuankiet.work@gmail.com>
2024-11-17 09:00:28 +0700

    change logo

--- config.py
+++ config.py
@@ -1,20 +1,6 @@
 import streamlit as st
 
-LOGO = 'https://www.fit.hcmus.edu.vn/assets/img/logos/fit-logo.png'
-NO_CHUNKING = "No Chunking"
-CHUNK = 'chunk'
-GEMINI = 'gemini'
-OPENAI = 'openai'
-ONLINE_LLM = 'online_llm'
-LOCAL_LLM = 'local_llm'
-EN = 'en'
-ENGLISH = "English"
-VIETNAMESE = "Vietnamese"
-VI = 'vi'
-NONE = 'none'
-USER = 'user'
-ASSISTANT = 'assistant'
-DEFAULT_LOCAL_LLM = 'hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF'
-DB = 'DB'
+LOGO = 'https://i.ibb.co/12vz6vQ/logo-remove-bg.png'
+# LOGO = 'asset/logo truong.png'
 OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
 GREETING='Xin chào! Tôi có thể giúp gì cho bạn?'
