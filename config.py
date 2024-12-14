68c7d73bc0245aa631dc58b1f95e9957c08268f4
kietvan <vantuankiet.work@gmail.com>
2024-12-14 12:47:16 +0700

    update app name

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-import streamlit as st
+import streamlit as st # type: ignore
 
 LOGO = 'https://i.ibb.co/12vz6vQ/logo-remove-bg.png'
 # LOGO = 'asset/logo truong.png'
 OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
 GREETING='Xin chào! Tôi có thể giúp gì cho bạn?'
+# LANGUAGE = st.session_state['language']
